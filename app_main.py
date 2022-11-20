835d57f431b4a85790df16e2a4398305c5fcbd52
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2022-11-20 13:26:34 -0600

    feat: add message data & move notebook to .py

--- app_main.py
+++ app_main.py
@@ -5,60 +5,13 @@ from datetime import datetime
 from typing import List
 import streamlit as st
 import plotly.express as px
-
-DATA_PATH = Path('data') / '53526472'
-MESSAGE_PATH = DATA_PATH / 'message.json'
-CONVO_PATH = DATA_PATH / 'conversation.json'
-
-with MESSAGE_PATH.open(encoding='utf-8') as f:
-    msg_data = json.load(f)
-    df_data_msg = pd.DataFrame(msg_data)
-
-with CONVO_PATH.open(encoding='utf-8') as f:
-    convo_data:dict = json.load(f)
-    # df_convo= pd.DataFrame(convo_data)
-df_data_msg.head(2)
-members:dict = convo_data['members']
-df_members = pd.DataFrame(members)
-df_members.head(3)
-id_map:pd.Series = df_members[['user_id','name']].set_index('user_id')['name']
-df_id_map = df_members[['user_id','name']]
-# How many muted?
-# df_members[df_members.muted == True]
-# df_data_msg[df_data_msg.system]
-
-def has_image(data:List[dict]) -> bool:
-    contains_image = False   
-    for dicky in data:
-       if dicky['type'] == 'image':
-           contains_image = True
-    return contains_image
-# Attachmeent ype?
-
-# print(len(df_data_msg))
-COLS_TO_KEEP = ['attachments', 'created_at', 'favorited_by', 'name',  'sender_type', 'system','text', 'user_id', 'event']
-df = df_data_msg[COLS_TO_KEEP].copy()
-df = df[df.system == False].copy() # Don't care about system messages for this analysis.
-df['created_at'] = df.created_at.apply(datetime.utcfromtimestamp)
-df['like_count'] = df.favorited_by.apply(len)
-df['has_attachement'] = df.attachments.apply( lambda x: len(x) > 0)
-df['attachement_num'] = df.attachments.apply( lambda x: len(x))
-df['has_image'] = df.attachments.apply(has_image)
-df['char_count'] = df.text.apply(lambda x: len(x) if not x is None else False )
-df['date_month'] = pd.PeriodIndex(year=df['created_at'].dt.year,month=df['created_at'].dt.month,freq='M')
-# Add date cols:
-
-df = df.merge(df_members[['user_id','nickname','name']],on='user_id',validate='m:1',how='left')
-
-COLS_TO_DROP = ['system']
-df = df.drop(columns=COLS_TO_DROP)
+from data_cleaning import GroupMeData
 
 
 
-d = df.groupby('name_y')['char_count'].mean().sort_values(ascending=False).to_frame().iloc[:10].reset_index()
+data = GroupMeData()
 
-fig = px.bar(d,x='name_y',y='char_count')
 
-st.plotly_chart(fig)
-st.write(d)
-print('done')
+
+
+print('lol')
---
+++ data_cleaning.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+import json
+import pandas as pd
+from datetime import datetime
+from typing import List
+import streamlit as st
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+
+
+
+class GroupMeData:
+    DATA_PATH = Path('data') / '53526472'
+    MESSAGE_PATH = DATA_PATH / 'message.json'
+    CONVO_PATH = DATA_PATH / 'conversation.json'   
+    ENCODING = 'utf-8'
+
+    def __init__(self,load_data=True) -> None:
+        self.msg_data = {}
+
+        if load_data:
+            self.load_all_data()
+
+    def load_all_data(self):
+        self.load_convo_data()
+        self.load_message_data()
+
+    def load_message_data(self):
+        with self.MESSAGE_PATH.open(encoding=self.ENCODING) as f:
+            self.msg_data:dict = json.load(f)
+        
+        self.df_data_msg = pd.DataFrame(self.msg_data)
+
+    def load_convo_data(self):
+        with self.CONVO_PATH.open(encoding=self.ENCODING) as f:
+            self.convo_data:dict = json.load(f)
+      
+        self.members_data:dict = self.convo_data['members']
+        self.df_members = pd.DataFrame(self.members_data)
+        self.id_map:pd.Series = self.df_members[['user_id','name']].set_index('user_id')['name']
+        self.df_id_map = self.df_members[['user_id','name']]
+
+    def clean_data(self):
+
+        MSG_COLS_TO_KEEP = ['attachments', 'created_at', 'favorited_by', 'name',  'sender_type', 'system','text', 'user_id', 'event']        
+        df = self.msg_data[MSG_COLS_TO_KEEP].copy()
+        df = df[df.system == False].copy() # Don't care about system messages for this analysis.
+        df['created_at'] = df.created_at.apply(datetime.utcfromtimestamp)
+        df['like_count'] = df.favorited_by.apply(len)
+        df['has_attachement'] = df.attachments.apply( lambda x: len(x) > 0)
+        df['attachement_num'] = df.attachments.apply( lambda x: len(x))
+        df['has_image'] = df.attachments.apply(has_image)
+        df['char_count'] = df.text.apply(lambda x: len(x) if not x is None else False )
+        df['date_month'] = pd.PeriodIndex(year=df['created_at'].dt.year,month=df['created_at'].dt.month,freq='M')
+        df = df.rename(columns={'name':'msg_name'}) # So we don't get it confused with the permanent name in convo data
+        # Combine with member data:
+        df = df.merge(self.df_members[['user_id','nickname','name']],on='user_id',validate='m:1',how='left')
+        COLS_TO_DROP = ['system']
+        df.drop(columns=COLS_TO_DROP)       
+
+        self.df_msg = df
+
+
+def has_image(data:List[dict]) -> bool:
+    contains_image = False   
+    for dicky in data:
+       if dicky['type'] == 'image':
+           contains_image = True
+    return contains_image
+
