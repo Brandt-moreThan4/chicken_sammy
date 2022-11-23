e3d2e3784c41f5653a8a4eca4346fdca1c69558c
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2022-11-22 20:44:22 -0600

    feat: add ability to see image for a message

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,8 +7,15 @@ import streamlit as st
 import plotly.express as px
 import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
+from typing import List
 
 
+def has_image(data:List[dict]) -> bool:
+    contains_image = False   
+    for dicky in data:
+       if dicky['type'] == 'image':
+           contains_image = True
+    return contains_image
 
 class DataCalcs:
     def calculate_properties(self):
@@ -16,8 +23,8 @@ class DataCalcs:
         self.msg_count = len(self.df_msg)
         self.member_names = self.df_members.name.unique().tolist()
 
-    def most_liked_msg(self) -> pd.Series:
-        return self.df_msg.sort_values('like_count').iloc[-1]
+    def most_liked_msg(self) -> "Message":
+        return Message(self.df_msg.sort_values('like_count').iloc[-1])
 
 
 class DataWrangler(DataCalcs):
@@ -59,49 +66,83 @@ class DataWrangler(DataCalcs):
 
     def clean_data(self):
 
-        MSG_COLS_TO_KEEP = ['attachments', 'created_at', 'favorited_by', 'name',  'sender_type', 'system','text', 'user_id', 'event']        
+        MSG_COLS_TO_KEEP = ['created_at','name','text','attachments', 'favorited_by', 'sender_type', 'system', 'user_id', 'event','id']        
         df = self.df_data_msg[MSG_COLS_TO_KEEP].copy()
         df = df[df.system == False].copy() # Don't care about system messages for this analysis.
+        df = df[df.sender_type == 'user'].copy() # Only look at users
         df['created_at'] = df.created_at.apply(datetime.utcfromtimestamp)
         df['like_count'] = df.favorited_by.apply(len)
         df['has_attachement'] = df.attachments.apply( lambda x: len(x) > 0)
         df['attachement_num'] = df.attachments.apply( lambda x: len(x))
         df['has_image'] = df.attachments.apply(has_image)
         df['char_count'] = df.text.apply(lambda x: len(x) if not x is None else False )
-        df['date_month'] = pd.PeriodIndex(year=df['created_at'].dt.year,month=df['created_at'].dt.month,freq='M')
-        df = df.rename(columns={'name':'msg_name'}) # So we don't get it confused with the permanent name in convo data
+        # df['date_month'] = pd.PeriodIndex(year=df['created_at'].dt.year,month=df['created_at'].dt.month,freq='M')
+        df = df.rename(columns={'name':'msg_name','id':'msg_id'}) # Be more explicit
+
         # Combine with member data:
         df = df.merge(self.df_members[['nickname','name']],left_on='user_id',right_index=True,validate='m:1',how='left')
-        COLS_TO_DROP = ['system']
-        df.drop(columns=COLS_TO_DROP)       
+        COLS_TO_DROP = ['system','sender_type']
+        df = df.drop(columns=COLS_TO_DROP)       
 
         self.df_msg = df
 
 
+gdata = DataWrangler()
 
 
-class PersonData(DataCalcs):
-    def __init__(self,user_id:str,data_all:DataWrangler) -> None:
+class Person(DataCalcs):
+    def __init__(self,user_id:str,data_all:DataWrangler=gdata) -> None:
+
         self.user_id = user_id
-
         df_msg_all = data_all.df_msg
         self.df_msg = df_msg_all[df_msg_all.user_id == user_id]
         self.member_info = data_all.df_members[data_all.df_members.index == user_id]
         self.name = self.member_info.name
     
 
+class Message:
+    def __init__(self,row:pd.Series) -> None:
+        self.text:str = row['text']
+
+        self.created_at:datetime = row['created_at']
+        self.sender_name: str = row['msg_name']
+        self.sender_id:str = row['user_id']
+        # self.person = Person(row['user_id'])
+        self.msg_id = row['msg_id']
+        self.likes:int = row['like_count']
+        self._permname:str = row['name']
+        self.nickname:str = row['nickname']
+        self.liked_by:List[str] = row['favorited_by']
+        self.char_count:int = row['char_count']
+        self.has_image:bool = row['has_image']
+        self.attachments:list[Attachment] = [Attachment(data) for data in row['attachments']]
+
+    @property
+    def images(self) -> list["Attachment"]:
+        return [attachment for attachment in self.attachments if attachment.is_image]
 
 
-def has_image(data:List[dict]) -> bool:
-    contains_image = False   
-    for dicky in data:
-       if dicky['type'] == 'image':
-           contains_image = True
-    return contains_image
+
+class Attachment:
+    def __init__(self,attachment:dict) -> None:
+        self.data = attachment
+        self.type:str = attachment['type']
+        self.is_image:bool = False
+
+        if self.type == 'image':
+             self.is_image:bool = True
+             self.image_url:str = attachment['url']
+             self.image_id:str = self.extract_imag_id(self.image_url)
+
+    @staticmethod
+    def extract_imag_id(image_url:str) -> str:
+        return image_url[(image_url.rfind('.')+1):]
 
 
-gdata = DataWrangler()
 
-person = PersonData('5994102',gdata)
+
+
+person = Person('5994102',gdata)
+msg = person.most_liked_msg()
 
 print('lol')
--- pages/Individual_Stats.py
+++ pages/Individual_Stats.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from data_cleaning import gdata, PersonData
+from data_cleaning import gdata, Person
 import pandas as pd
 import streamlit.components.v1 as components
 
@@ -13,7 +13,7 @@ name_select = st.selectbox('Who who you like to get deep into?',members)
 st.write('Excellent choice, you selected', name_select)
 
 user_id = gdata.id_map[gdata.id_map == name_select].index[0]
-person = PersonData(user_id,gdata)
+person = Person(user_id,gdata)
 
 st.markdown('#### Basic Stats')
 # Active Since
@@ -22,12 +22,18 @@ st.markdown('#### Basic Stats')
 st.markdown("#### Your Most Liked Message")
 msg = person.most_liked_msg()
 msg_text = f'<i><q>{msg.text}</q></i>'
-components.html(msg_text)
+components.html(msg_text,scrolling=True,height=150)
+if msg.has_image:
+    # Grab the first image for now:
+    img = msg.images[0]
+    img_html = f''' <img height="300" src="{img.image_url}" alt="Sammy Image"> '''
+    components.html(img_html,height=300)
+
 # st.write(msg.text)
 # st.info(msg.text)
 # st.success(msg.text)
 
-st.write(msg.to_frame())
+# st.write(msg.to_frame())
 
 st.markdown('#### Your most liked person')
 st.markdown('#### Person who likes you the most')
