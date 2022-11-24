8128a48733a7c14368b729446c8d427f4e7a3409
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2022-11-24 08:59:51 -0600

    feat: adding spacy

--- pages/Experiments.py
+++ pages/Experiments.py
@@ -2,22 +2,9 @@ import streamlit as st
 import datetime
 from data_cleaning import data_all
 
-d_start = st.date_input(
-    "Enter Start Date",
-    datetime.date(2019, 7, 6))
 
-st.write('Your birthday is:', d_start)
+# from st_aggrid import AgGrid
+# import pandas as pd
 
-def convert_df(df):
-    return df.to_csv().encode('utf-8')
-
-
-csv = convert_df(data_all.df_members)
-
-st.download_button(
-    "Press to Download",
-    convert_df(data_all.df_members),
-    "members.csv",
-    "text/csv",
-    key='browser-data'
-)
+# df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
+# AgGrid(df)
