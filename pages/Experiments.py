20b7b59e6cb0ccee533bf1dc05c3be1821f9661a
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2022-11-23 22:50:53 -0600

    feat: added download data ability

--- pages/Experiments.py
+++ pages/Experiments.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import datetime
+from data_cleaning import data_all
 
 d_start = st.date_input(
     "Enter Start Date",
@@ -7,3 +8,16 @@ d_start = st.date_input(
 
 st.write('Your birthday is:', d_start)
 
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
+
+csv = convert_df(data_all.df_members)
+
+st.download_button(
+    "Press to Download",
+    convert_df(data_all.df_members),
+    "members.csv",
+    "text/csv",
+    key='browser-data'
+)
--- pages/Raw_Data.py
+++ pages/Raw_Data.py
@@ -1,13 +1,31 @@
 
 import streamlit as st
 from data_cleaning import data_all
+import pandas as pd
 
 
+def convert_df(df:pd.DataFrame):
+    return df.to_csv().encode('utf-8')
 
 st.markdown('## Member Data')
 st.write(data_all.df_members)
 
-st.markdown('## MSG Data: (Bug here)')
+st.download_button(
+    "Press to Download Member Data",
+    convert_df(data_all.df_members),
+    "members.csv",
+    "text/csv",
+    key='member-data'
+)
+
+st.markdown('## MSG Data: (Bug in Displayhere)')
+st.download_button(
+    "Press to Download Message Data",
+    convert_df(data_all.df_msg),
+    "messages.csv",
+    "text/csv",
+    key='msg-data'
+)
 # st.write(gdata.df_msg)
 
 
