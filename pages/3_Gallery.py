4b208ee9e914ad23ca07fcefd67eb8da4e6b1fd4
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2022-11-24 16:33:29 -0600

    feat: fix gallery

--- pages/3_Gallery.py
+++ pages/3_Gallery.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import streamlit as st
-import streamlit.components.v1 as components
+from PIL import Image
 
 
 st.markdown('## Coming Soon')
 st.markdown('#### A Small Taste:')
 
-components.html('<img height="300" src="https://github.com/Brandt-moreThan4/chicken_sammy/blob/v2/data/sample_images/156842797118905542_1200x1200.437da0a46bc84c24a4e5a359e3fe071c.jpeg?raw=true" alt="Sammy Image">',height=300)
+# components.html('<img height="300" src="sammy_icon2.png" alt="Sammy Image">',height=300)
+st.image(Image.open('sammy_icon2.png'))
 
 
 st.markdown('#### Upload your own')
